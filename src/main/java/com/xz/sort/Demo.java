7957b11dcc5a7e94ab90ff66381fd23b3240214d
xiezhe <xiezhe@haoyuanyiliao.com>
2020-05-25 20:53:31 +0800

    快速排序

--- src/main/java/com/xz/sort/Demo.java
+++ src/main/java/com/xz/sort/Demo.java
@@ -17,9 +17,10 @@ public class Demo {
 
     static {
         Random random = new Random();
+//        arr = new int[]{438, 420, 96, 264, 101, 181, 346, 87, 352, 135};
         arr = new int[maxSize];
         for (int i = 0; i < maxSize; i++) {
-            arr[i] = random.nextInt(maxSize * 5);
+            arr[i] = random.nextInt(maxSize * 50);
         }
     }
 
@@ -28,8 +29,10 @@ public class Demo {
 //        bubbleSort(); //8W数据 10578ms
 //        selectionSort();//8W数据 2500ms
 //        insertSort();//8W数据 500ms
-        shellSort();//8W数据 30ms
+//        shellSort();//8W数据 30ms
+        quickSort(0, arr.length - 1);
         System.out.println(System.currentTimeMillis() - start);
+        System.out.println(Arrays.toString(arr));
     }
 
     /**
@@ -98,12 +101,12 @@ public class Demo {
     /**
      * 希尔插入法
      */
-    private static void shellSort(){
+    private static void shellSort() {
         int step = arr.length / 2;
-        while(step != 0){
+        while (step != 0) {
             int value;
             int index;
-            for(int i = step ; i < arr.length ; i++){
+            for (int i = step; i < arr.length; i++) {
 //                for(int j = i - step ; j>=0 ; j -= step){
 //                    if(arr[j] > arr[j+step]){
 //                        swap(j,j+step);
@@ -112,7 +115,7 @@ public class Demo {
                 index = i;
                 value = arr[i];
                 //按步进直接插入
-                while(index - step >= 0 && arr[index - step] > value){
+                while (index - step >= 0 && arr[index - step] > value) {
                     arr[index] = arr[index - step];
                     index -= step;
                 }
@@ -123,9 +126,50 @@ public class Demo {
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     * 快速排序
+     *
+     * @param start
+     * @param end
+     */
+    private static void quickSort(int start, int end) {
+        //基准值
+        int pivot = arr[start];
+        //开始位置
+        int i = start;
+        //结束位置
+        int j = end;
+        while (i < j) {
+            //从后面开始找，直到找到一个比基准小的
+            while ((i < j) && (arr[j] > pivot)) {
+                j--;
+            }
+            //从前面开始找，直到找到比基准大的
+            while ((i < j) && (arr[i] < pivot)) {
+                i++;
+            }
+            //和基准值相等，下标移位，继续找
+            if ((arr[i] == arr[j]) && (i < j)) {
+                i++;
+                j--;
+            } else {
+                //交换数据
+                swap(i,j);
+            }
+        }
+        //左递归
+        if (i - 1 > start) {
+            quickSort(start, i - 1);
+        }
+        //右递归
+        if (j + 1 < end) {
+            quickSort(j + 1, end);
+        }
+    }
+
     private static void swap(int x, int y) {
-        arr[x] = arr[x] + arr[y];
-        arr[y] = arr[x] - arr[y];
-        arr[x] = arr[x] - arr[y];
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
     }
 }
